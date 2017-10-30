d712299c941f1320172af869990ded9e6fbf13f1
ianwalker9401 <ianwalker9401@gmail.com>
2017-10-30 18:49:24 -0400

    changes

--- Stock-o-Matic3000/app/src/main/java/no_idea/stock_o_matic3000/Control.java
+++ Stock-o-Matic3000/app/src/main/java/no_idea/stock_o_matic3000/Control.java
@@ -1,36 +1,56 @@
 package no_idea.stock_o_matic3000;
 
 public class Control{
-    public FoodList foods;
+    private static FoodList foods;
     private RecipeList recipes;
  
     public Control(){
         foods = new FoodList();
         recipes = new RecipeList();
-
-        foods.addItem(new FoodItem("Apple", 4, 9, 24, 2017));
-        foods.addItem(new FoodItem("Orange", 4, 9, 29, 2017));
-        foods.addItem(new FoodItem("Sausage", 3, 9, 25, 2017));
-        foods.addItem(new FoodItem("Potato", 6, 9, 30, 2017));
-        foods.addItem(new FoodItem("Butter", 1, 9, 12, 2017));
-        foods.addItem(new FoodItem("Steak", 1, 9, 14, 2017));
-        foods.addItem(new FoodItem("Tomato", 2, 9, 15, 2017));
-        foods.addItem(new FoodItem("Celery", 3, 9, 1, 2017));
-        foods.addItem(new FoodItem("Mustard", 1, 9, 6, 2017));
-        foods.addItem(new FoodItem("Bread", 1, 9, 7, 2017));
+        generate();
     }
  
     public void generate(){
-        foods.addItem(new FoodItem("Apple", 4, 9, 24, 2017));
-        foods.addItem(new FoodItem("Orange", 4, 9, 29, 2017));
-        foods.addItem(new FoodItem("Sausage", 3, 9, 25, 2017));
-        foods.addItem(new FoodItem("Potato", 6, 9, 30, 2017));
-        foods.addItem(new FoodItem("Butter", 1, 9, 12, 2017));
-        foods.addItem(new FoodItem("Steak", 1, 9, 14, 2017));
-        foods.addItem(new FoodItem("Tomato", 2, 9, 15, 2017));
-        foods.addItem(new FoodItem("Celery", 3, 9, 1, 2017));
-        foods.addItem(new FoodItem("Mustard", 1, 9, 6, 2017));
-        foods.addItem(new FoodItem("Bread", 1, 9, 7, 2017));
+        FoodItem apple = new FoodItem("Apple", 4, 9, 24, 2017);
+        FoodItem orange = new FoodItem("Orange", 4, 9, 29, 2017);
+        FoodItem sausage = new FoodItem("Sausage", 3, 9, 25, 2017);
+        FoodItem potato = new FoodItem("Potato", 6, 9, 30, 2017);
+        FoodItem butter = new FoodItem("Butter", 1, 9, 12, 2017);
+        FoodItem steak = new FoodItem("Steak", 1, 9, 14, 2017);
+        FoodItem tomato = new FoodItem("Tomato", 2, 9, 15, 2017);
+        FoodItem celery = new FoodItem("Celery", 3, 9, 1, 2017);
+        FoodItem mustard = new FoodItem("Mustard", 1, 9, 6, 2017);
+        FoodItem bread = new FoodItem("Bread", 1, 9, 7, 2017);
+
+        foods.addItem(apple);
+        foods.addItem(orange);
+        foods.addItem(sausage);
+        foods.addItem(potato);
+        foods.addItem(butter);
+        foods.addItem(steak);
+        foods.addItem(tomato);
+        foods.addItem(celery);
+        foods.addItem(mustard);
+        foods.addItem(bread);
+
+        Recipe a = new Recipe();
+        a.setName("Butter Steak");
+        a.addIngredient(steak);
+        a.addIngredient(butter);
+       /* a.addInstruction("Melt butter in pan");
+        a.addInstruction("Fry steak in buttery pan");
+        a.addInstruction("Do the fancy steak stuff like letting it rest or whatever");
+        a.addInstruction("Eat the steak you carnivore");*/
+
+        Recipe b = new Recipe();
+        b.setName("Slightly Warm Apple");
+        b.addIngredient(apple);
+        b.addIngredient(butter);
+        b.addIngredient(celery);
+        b.addIngredient(tomato);
+        /*b.addInstruction("Throw out everything except the apple");
+        b.addInstruction("Microwave the apple for a small amount of time");
+        b.addInstruction("Consume your slightly warm apple");*/
     }
     // will take in a food name and expiry date and place it in the food list specified.
     public void addFood(String name, String numItem, String date){
--- Stock-o-Matic3000/app/src/main/java/no_idea/stock_o_matic3000/Recipe.java
+++ Stock-o-Matic3000/app/src/main/java/no_idea/stock_o_matic3000/Recipe.java
@@ -7,69 +7,79 @@ import java.util.*;
  *  Purpose: Holds components of *
  *           a Recipe            *
  *                               *
-*********************************/
+ *********************************/
 
 public class Recipe{
- private FoodList ingredients;
- private ArrayList<String> instructions;
- private int numIngredients;
- /*********************************
-  *                               *
-  *       Constructors            *
-  *                               *
-  *********************************/
- 
- //Empty Constructor
- public Recipe(){
-  ingredients = new FoodList();
-  instructions = new ArrayList();
- }
- 
- //Constructor taking a food list
- public Recipe(FoodList ingredients){
-  this.ingredients = ingredients;
- }
- 
- //Contructor taking food list and instruction list
- public Recipe(FoodList ingredients, ArrayList instructions){
-  this.ingredients = ingredients;
-  this.instructions = instructions;
- }
- 
- /*********************************
-  *                               *
-  *      Methods                  *
-  *                               *
-  *********************************/
- 
- //adds specified ingredient
- public void addIngredient(FoodItem ingredient){
-     ingredients.addItem(ingredient);
-     numIngredients = ingredients.getSize();
- }
- 
- //removes specified ingredient
- public void removeIngredient(FoodItem ingredient){
-     ingredients.removeItem(ingredient);
- }
- 
- //removes ingredient specified by index
- public void removeIngredient(int itemNum){
-     ingredients.removeItem(itemNum);
- }
- 
- //adds an instruction
- public void addInstruction(String instruction){
-     instructions.add(instruction);
- }
- 
- //gets all instructions
- public ArrayList getInstructions(){
-     return instructions;
- }
- 
- //gets specific instruction
- public String getInstruction(int instrucNum){
-     return instructions.get(instrucNum);
- }
+     private String name;
+     private FoodList ingredients;  //list of required ingredients
+     //private ArrayList<String> instructions;    //list of instructions
+     private Boolean hasAllItems;   //if the recipe has all the ingredients or not (f is no)
+     private int numIngredients;    //number of ingredients
+     /*********************************
+      *                               *
+      *       Constructors            *
+      *                               *
+      *********************************/
+
+     //Empty Constructor
+     public Recipe(){
+      ingredients = new FoodList();
+      //instructions = new ArrayList();
+     }
+
+     //Constructor taking a food list
+     public Recipe(FoodList ingredients){
+      this.ingredients = ingredients;
+     }
+
+     //Constructor taking food list and instruction list
+     /*public Recipe(FoodList ingredients){
+      this.ingredients = ingredients;
+      //this.instructions = instructions;
+     }*/
+
+     /*********************************
+      *                               *
+      *      Methods                  *
+      *                               *
+      *********************************/
+
+     //adds specified ingredient
+     public void addIngredient(FoodItem ingredient){
+         ingredients.addItem(ingredient);
+         numIngredients = ingredients.getSize();
+     }
+     public void setName(String newName){
+         name = newName;
+     }
+
+     public String getName(){
+         return name;
+     }
+     //removes specified ingredient
+     public void removeIngredient(FoodItem ingredient){
+         ingredients.removeItem(ingredient);
+     }
+
+     //removes ingredient specified by index
+     public void removeIngredient(int itemNum){
+         ingredients.removeItem(itemNum);
+     }
+
+     //adds an instruction
+     /* public void addInstruction(String instruction){
+         instructions.add(instruction);
+     }
+
+     //gets all instructions
+     public ArrayList getInstructions(){
+         return instructions;
+     }
+
+     //gets specific instruction
+     public String getInstruction(int instrucNum){
+         return instructions.get(instrucNum);
+     }
+
+     //sets an instruction (for editing instructions)*/
 }
